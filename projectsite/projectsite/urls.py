ea1f982609d5269224535aa4eaf9ddc1fe29bc08
AdrianGabuco <adriangabucoo@gmail.com>
2023-12-08 06:14:16 +0800

    Adjustments & Modifications

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -21,7 +21,7 @@ from cardquest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
     path('trainer_list', TrainerList.as_view(), name='trainer-list',
     )
 ]
